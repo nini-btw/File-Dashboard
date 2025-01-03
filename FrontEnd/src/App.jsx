98377e706b72f0e9cac9dd2588fca6116b66799b
nini-btw <mohamed01112002@gmail.com>
2025-01-03 19:30:52 +0100

    add Login Page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { lightTheme, darkTheme } from "./theme";
-import Header from "./components/Header";
+import Login from "./components/Login";
 import Footer from "./components/Footer";
 import "./main.sass";
+import ContrastIcon from "@mui/icons-material/Contrast";
+import { Box } from "@mui/material";
 
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -13,14 +15,24 @@ const App = () => {
 
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      {/* Basic Rules */}
       <CssBaseline /> {/* Add the GlobalStyles component here */}
-      <Header />
-      <main style={{ padding: "1rem" }}>
-        <h1>Welcome to My App</h1>
-        <button onClick={toggleTheme}>
-          Switch to {isDarkMode ? "Light" : "Dark"} Mode
-        </button>
-      </main>
+      <Box
+        onClick={toggleTheme}
+        sx={{
+          position: "absolute",
+          top: "0",
+          right: "0",
+          backgroundColor: "transparent",
+          color: "inherit",
+          zIndex: "1003",
+          cursor: "pointer",
+        }}
+      >
+        <ContrastIcon />
+      </Box>
+      {/* Main Components */}
+      <Login />
       <Footer />
     </ThemeProvider>
   );
