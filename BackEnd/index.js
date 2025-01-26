0fa1d30d939dc268a3862ea48ff3db409a71d6b2
nini-btw <mohamed01112002@gmail.com>
2025-01-26 02:16:38 +0100

    second commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,1 +1,23 @@
-console.log("hi");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+//creating the server
+const app = express();
+
+//Connecting to The DataBase
+const mongoose = require("mongoose");
+const url = process.env.DB_URL;
+const port = process.env.PORT;
+
+mongoose
+  .connect(url)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(() => console.error("MongoDB connection error"));
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log("listening on PORT 5000");
+});
